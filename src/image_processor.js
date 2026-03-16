820afba7126d8ee9d8f1d758dfe74326858d8c2c
= <skaftasoneric@gmail.com>
2026-03-15 22:01:26 -0400

    restructured code

---
+++ src/image_processor.js
@@ -0,0 +1,132 @@
+class ImageProcessor {
+    #src;
+    #canvas;
+    #ctx;
+
+    // includes metedata
+    #imageData;
+
+    constructor(src) {
+        this.#src = src;
+
+        this.#initCanvas();
+    }
+
+    #initCanvas() {
+        const img = new Image();
+        img.src = this.#src;
+
+        this.#canvas = document.createElement('canvas');
+        this.#ctx = this.#canvas.getContext('2d');
+
+        img.onload = () => {
+            this.#canvas.width = img.width;
+            this.#canvas.height = img.height;
+
+            this.#ctx.drawImage(img, 0, 0);
+
+            this.#imageData = this.#ctx.getImageData(0, 0, img.width, img.height);
+        };
+    }
+
+    getImageData() {
+        return structuredClone(this.#imageData);
+    }
+
+    getPixelData_RGBA(x, y) {
+        // data is a Uint8ClampedArray: [R, G, B, A, R, G, B, A, ...]
+
+        const pixelStartIndex = (y * this.#imageData.data.width + x) * 4;
+        
+        return [
+            this.#imageData[pixelStartIndex],
+            this.#imageData[pixelStartIndex + 1],
+            this.#imageData[pixelStartIndex + 2],
+            this.#imageData[pixelStartIndex + 3]
+        ];
+    }
+
+    getPixelData_lum(x, y) {
+        const rgba = this.getPixelData_RGBA(x, y);
+        const luminance = (rgba[0] + rgba[1] + rgba[2]) / 3;
+
+        return [luminance];
+    }
+
+    getImageData_lum() {
+        const imageData = this.getImageData();
+
+        if (!imageData) return;
+
+        // Iterate over each pixel
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            // Iterate over [r, g, b], but skip [a] for each pixel
+            let sum = 0;
+
+            for (let j = 0; j < 3; j++) {
+                sum += imageData.data[i + j];
+            }
+
+            const avg = Math.round(sum / 3);
+            
+            // Set the r, g, b values to the avg - chrominance
+            imageData.data[i] = avg;
+            imageData.data[i + 1] = avg;
+            imageData.data[i + 2] = avg;
+        }
+
+        return imageData;
+    }
+
+    getImageData_BW(threshold = 128) {
+        const imageData = this.getImageData_lum();
+
+        if (!imageData) return;
+
+        // Iterate over each pixel
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            // Since RGB values for greyscale images are equal, the R value can be used as the chrominance value
+            let chrominance = imageData.data[i];
+
+            chrominance >= threshold ? chrominance = 255 : chrominance = 0;
+            
+            // Set the r, g, b values to the avg - chrominance
+            imageData.data[i] = chrominance;
+            imageData.data[i + 1] = chrominance;
+            imageData.data[i + 2] = chrominance;
+        }
+
+        return imageData;
+    }
+
+    // Random dithering
+    getImageData_dithered(min, max) {
+        const imageData = this.getImageData_lum();
+
+        const spread = max - min;
+
+        if (!imageData) return;
+
+        // Iterate over each pixel
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            // Since RGB values for greyscale images are equal, the R value can be used as the chrominance value
+            let chrominance = imageData.data[i];
+
+            const threshold = min + Math.floor(Math.random() * (spread + 1));
+
+            chrominance >= threshold ? chrominance = 255 : chrominance = 0;
+            
+            // Set the r, g, b values to the avg - chrominance
+            imageData.data[i] = chrominance;
+            imageData.data[i + 1] = chrominance;
+            imageData.data[i + 2] = chrominance;
+        }
+
+        return imageData;
+    }
+
+    // One-Dimensional Error Diffusion Dithering
+
+}
+
+export { ImageProcessor };
--- src/index.js
+++ src/index.js
@@ -1,147 +1,18 @@
-class ImageDataHelper {
+import { ImageProcessor } from "./image_processor.js";
+
+class ImageRenderer {
     #src;
     #canvas;
     #ctx;
 
-    // includes metedata
-    #imageData;
-
-    constructor(src) {
-        this.#src = src;
-
-        this.#initCanvas();
-    }
-
-    #initCanvas() {
-        const img = new Image();
-        img.src = this.#src;
-
-        this.#canvas = document.createElement('canvas');
-        this.#ctx = this.#canvas.getContext('2d');
-
-        img.onload = () => {
-            this.#canvas.width = img.width;
-            this.#canvas.height = img.height;
-
-            this.#ctx.drawImage(img, 0, 0);
-
-            this.#imageData = this.#ctx.getImageData(0, 0, img.width, img.height);
-        };
-    }
-
-    getImageData() {
-        return structuredClone(this.#imageData);
-    }
-
-    getPixelData_RGBA(x, y) {
-        // data is a Uint8ClampedArray: [R, G, B, A, R, G, B, A, ...]
-
-        const pixelStartIndex = (y * this.#imageData.data.width + x) * 4;
-        
-        return [
-            this.#imageData[pixelStartIndex],
-            this.#imageData[pixelStartIndex + 1],
-            this.#imageData[pixelStartIndex + 2],
-            this.#imageData[pixelStartIndex + 3]
-        ];
-    }
-
-    getPixelData_lum(x, y) {
-        const rgba = this.getPixelData_RGBA(x, y);
-        const luminance = (rgba[0] + rgba[1] + rgba[2]) / 3;
-
-        return [luminance];
-    }
-
-    getImageData_lum() {
-        const imageData = this.getImageData();
-
-        if (!imageData) return;
-
-        // Iterate over each pixel
-        for (let i = 0; i < imageData.data.length; i += 4) {
-            // Iterate over [r, g, b], but skip [a] for each pixel
-            let sum = 0;
-
-            for (let j = 0; j < 3; j++) {
-                sum += imageData.data[i + j];
-            }
-
-            const avg = Math.round(sum / 3);
-            
-            // Set the r, g, b values to the avg - chrominance
-            imageData.data[i] = avg;
-            imageData.data[i + 1] = avg;
-            imageData.data[i + 2] = avg;
-        }
-
-        return imageData;
-    }
-
-    getImageData_BW(threshold = 128) {
-        const imageData = this.getImageData_lum();
-
-        if (!imageData) return;
-
-        // Iterate over each pixel
-        for (let i = 0; i < imageData.data.length; i += 4) {
-            // Since RGB values for greyscale images are equal, the R value can be used as the chrominance value
-            let chrominance = imageData.data[i];
-
-            chrominance >= threshold ? chrominance = 255 : chrominance = 0;
-            
-            // Set the r, g, b values to the avg - chrominance
-            imageData.data[i] = chrominance;
-            imageData.data[i + 1] = chrominance;
-            imageData.data[i + 2] = chrominance;
-        }
-
-        return imageData;
-    }
-
-    // Random dithering
-    getImageData_dithered(min, max) {
-        const imageData = this.getImageData_lum();
-
-        const spread = max - min;
-
-        if (!imageData) return;
-
-        // Iterate over each pixel
-        for (let i = 0; i < imageData.data.length; i += 4) {
-            // Since RGB values for greyscale images are equal, the R value can be used as the chrominance value
-            let chrominance = imageData.data[i];
-
-            const threshold = min + Math.floor(Math.random() * (spread + 1));
-
-            chrominance >= threshold ? chrominance = 255 : chrominance = 0;
-            
-            // Set the r, g, b values to the avg - chrominance
-            imageData.data[i] = chrominance;
-            imageData.data[i + 1] = chrominance;
-            imageData.data[i + 2] = chrominance;
-        }
-
-        return imageData;
-    }
-
-    // One-Dimensional Error Diffusion Dithering
-
-}
-
-class FilteredImage {
-    #src;
-    #canvas;
-    #ctx;
-
-    #imageDataHelper;
+    #ImageProcessor;
 
     constructor(src) {
         // Define source
         this.#src = src;
 
-        // Initialise imageDataHelper
-        this.#imageDataHelper = new ImageDataHelper(src);
+        // Initialise ImageProcessor
+        this.#ImageProcessor = new ImageProcessor(src);
 
         // Create canvas
         this.#canvas = document.createElement('canvas');
@@ -160,7 +31,7 @@ class FilteredImage {
     }
 
     displayOriginal() {
-        const imageData = this.#imageDataHelper.getImageData();
+        const imageData = this.#ImageProcessor.getImageData();
         if (!imageData) {
             setTimeout(() => {
                 this.displayOriginal();
@@ -172,7 +43,7 @@ class FilteredImage {
     }
     
     displayGreyscale() {
-        const imageData = this.#imageDataHelper.getImageData_lum();
+        const imageData = this.#ImageProcessor.getImageData_lum();
         if (!imageData) {
             setTimeout(() => {
                 this.displayGreyscale();
@@ -186,7 +57,7 @@ class FilteredImage {
 
     // black and white
     displayBW(threshold = 128) {
-        const imageData = this.#imageDataHelper.getImageData_BW(threshold);
+        const imageData = this.#ImageProcessor.getImageData_BW(threshold);
         if (!imageData) {
             setTimeout(() => {
                 this.displayBW(threshold);
@@ -199,7 +70,7 @@ class FilteredImage {
     }
 
     displayDithered_rand(min = 0, max = 255) {
-        const imageData = this.#imageDataHelper.getImageData_dithered(min, max);
+        const imageData = this.#ImageProcessor.getImageData_dithered(min, max);
         if (!imageData) {
             setTimeout(() => {
                 this.displayDithered_rand(min, max);
@@ -236,7 +107,7 @@ document.querySelector('#apply_filter').addEventListener('click', () => {
     });
 
 
-    const filtered_image = new FilteredImage(img_url);
+    const filtered_image = new ImageRenderer(img_url);
     switch (filter) {
         case 'og':
             filtered_image.displayOriginal();
