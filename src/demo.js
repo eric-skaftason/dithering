22110f2f1268c1f34f4ead3a18c48d6c987a8977
Eric Skaftason <skaftasoneric@gmail.com>
2026-04-03 15:11:31 -0400

    demo setup

---
+++ src/demo.js
@@ -0,0 +1,173 @@
+import { ImageProcessor } from "./image_processor.js";
+
+class ImageRenderer {
+    #src;
+    #canvas;
+    #ctx;
+
+    #ImageProcessor;
+
+    constructor(src) {
+        // Define source
+        this.#src = src;
+
+        // Initialise ImageProcessor
+        this.#ImageProcessor = new ImageProcessor(src);
+
+        // Create canvas
+        this.#canvas = document.createElement('canvas');
+        this.#ctx = this.#canvas.getContext('2d');
+        this.#ctx.imageSmoothingEnabled = false;
+        document.body.append(this.#canvas);
+
+    }
+
+    #displayImage(imageData) {        
+        this.#canvas.width = imageData.width;
+        this.#canvas.height = imageData.height;
+
+        this.#ctx.putImageData(imageData, 0, 0);
+    }
+
+    displayOriginal() {
+        const imageData = this.#ImageProcessor.getImageData();
+        if (!imageData) {
+            setTimeout(() => {
+                this.displayOriginal();
+            }, 1000);
+            return;
+        }
+        
+        this.#displayImage(imageData);
+    }
+    
+    displayGreyscale() {
+        const imageData = this.#ImageProcessor.getImageData_lum();
+        if (!imageData) {
+            setTimeout(() => {
+                this.displayGreyscale();
+            }, 1000);
+            return;
+        }
+
+       this.#displayImage(imageData);
+
+    }
+
+    // black and white
+    displayBW(threshold = 128) {
+        const imageData = this.#ImageProcessor.getImageData_BW(threshold);
+        if (!imageData) {
+            setTimeout(() => {
+                this.displayBW(threshold);
+            }, 1000);
+            return;
+        }
+
+        this.#displayImage(imageData);
+
+    }
+
+    displayDithered_rand(min = 0, max = 255) {
+        const imageData = this.#ImageProcessor.getImageData_dithered(min, max);
+        if (!imageData) {
+            setTimeout(() => {
+                this.displayDithered_rand(min, max);
+            }, 1000);
+            return;
+        }
+
+        this.#displayImage(imageData);
+    }
+
+    displayDithered_errDiff(threshold = 128) {
+        const imageData = this.#ImageProcessor.getImageData_errDiff(threshold);
+        if (!imageData) {
+            setTimeout(() => {
+                this.displayDithered_errDiff(threshold);
+            }, 1000);
+            return;
+        }
+
+        this.#displayImage(imageData);
+    }
+
+    displayDithered_FloydSteinberg() {
+        const imageData = this.#ImageProcessor.getImageData_FloydSteinberg();
+        if (!imageData) {
+            setTimeout(() => {
+                this.displayDithered_FloydSteinberg();
+            }, 1000);
+            return;
+        }
+
+        this.#displayImage(imageData);
+    }
+}
+
+/*
+document.querySelector('#apply_filter').addEventListener('click', () => {
+    const img_input = document.querySelector('#img_input');
+    if (img_input.files.length === 0) {
+        alert('Please select an image.');
+        return;
+    }
+
+    const img_url = URL.createObjectURL(img_input.files[0]);
+
+    // Gets first instance of an input with anme "filters" that is checked
+    const filter_radio = document.querySelector('input[name="filters"]:checked');
+    if (filter_radio === null) {
+        alert('Please select a filter');
+        return;
+    }
+
+    const filter = filter_radio.getAttribute('value');
+
+    // Clear canvases
+    const canvases = document.querySelectorAll('canvas');
+    canvases.forEach(canvas => {
+        canvas.remove();
+    });
+
+
+    const filtered_image = new ImageRenderer(img_url);
+    switch (filter) {
+        case 'og':
+            filtered_image.displayOriginal();
+            break;
+        case 'grey':
+            filtered_image.displayGreyscale();
+            break;
+        case 'bw':
+            {
+                let threshold = 128;
+                threshold = Number(prompt('Input threshold: 0 - 255 (inclusive)'));
+                filtered_image.displayBW(threshold);
+            }
+            break;
+        case 'dither_rand':
+            {
+                let lower = 0;
+                let upper = 0;
+                lower = Number(prompt('Input lower bound: 0 - 255 (inclusive)'));
+                upper = Number(prompt('Input upper bound: 0 - 255 (inclusive)'));
+                filtered_image.displayDithered_rand(lower, upper);
+            }
+            break;
+        case 'err_diff':
+            {
+                let threshold = 128;
+                threshold = Number(prompt('Input threshold: 0 - 255 (inclusive)'));
+                filtered_image.displayDithered_errDiff(threshold);
+            }
+            break;
+        case 'floyd_steinberg':
+            {
+                filtered_image.displayDithered_FloydSteinberg();
+            }
+            break;
+
+    }
+});
+*/
