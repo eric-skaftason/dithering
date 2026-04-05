7373d4bc47ae6d77477b796aa72c8b62e46ffd08
Eric Skaftason <skaftasoneric@gmail.com>
2026-04-05 16:27:08 -0400

    ui changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 const demo_wrapper = document.querySelector('.demo_wrapper');
 
+// Minimise button
 document.querySelector('.minimise').addEventListener('click', () => {
     demo_wrapper.toggleAttribute('hidden');
+});
+
+function getImageInfo() {
+    const img_input = document.querySelector('#img_input');
+
+    return img_input.files[0];
+}
+
+// Select image
+const image_selector = document.querySelector('#sel_img');
+const file_input = document.querySelector('#img_input');
+
+image_selector.addEventListener('click', () => {
+    file_input.click();
+});
+file_input.addEventListener('change', () => {
+    image_selector.innerText = getImageInfo().name || image_selector.innerText;
+});
+
+// Select Filter
+const filter_selector = document.querySelector('#sel_filter');
+filter_selector.addEventListener('click', () => {
+    const menu = document.createElement('div');
+    menu.style.position = 'absolute';
+    menu.style.top = 0;
+    menu.style.left = 0;
+
+    const list_elements = 
+
+    for (let i = 0; i < 5; i++) {
+        const ele = document.createElement('dropdown-element');
+        menu.appendChild(ele);
+    }
+
+    demo_wrapper.append(menu);
 });
