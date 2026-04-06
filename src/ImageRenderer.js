8ee587d6ef076e6d692b40a86c80a4b4329d94aa
Eric Skaftason <skaftasoneric@gmail.com>
2026-04-05 22:40:23 -0400

    all display functions use new async handling, removed old commented-out code

--- src/ImageRenderer.js
+++ src/ImageRenderer.js
@@ -30,18 +30,6 @@ class ImageRenderer {
         ctx.putImageData(imageData, 0, 0);
     }
 
-    // displayOriginal(canvas) {
-    //     const imageData = this.#ImageProcessor.getImageData();
-    //     if (!imageData) {
-    //         setTimeout(() => {
-    //             this.displayOriginal(canvas);
-    //         }, 1000);
-    //         return;
-    //     }
-        
-    //     this.#displayImage(canvas, imageData);
-    // }
-
     async displayOriginal(canvas) {
         await this.ready;
         
@@ -49,135 +37,41 @@ class ImageRenderer {
         this.#displayImage(canvas, imageData);
     }
     
-    displayGreyscale(canvas) {
+    async displayGreyscale(canvas) {
+        await this.ready;
+
         const imageData = this.#ImageProcessor.getImageData_lum();
-        if (!imageData) {
-            setTimeout(() => {
-                this.displayGreyscale(canvas);
-            }, 1000);
-            return;
-        }
-
-       this.#displayImage(canvas, imageData);
-
+        this.#displayImage(canvas, imageData);
     }
 
     // black and white
-    displayBW(canvas, threshold = 128) {
+    async displayBW(canvas, threshold = 128) {
+        await this.ready;
+
         const imageData = this.#ImageProcessor.getImageData_BW(threshold);
-        if (!imageData) {
-            setTimeout(() => {
-                this.displayBW(canvas, threshold);
-            }, 1000);
-            return;
-        }
-
         this.#displayImage(canvas, imageData);
-
     }
 
-    displayDithered_rand(canvas, min = 0, max = 255) {
+    async displayDithered_rand(canvas, min = 0, max = 255) {
+        await this.ready;
+
         const imageData = this.#ImageProcessor.getImageData_dithered(min, max);
-        if (!imageData) {
-            setTimeout(() => {
-                this.displayDithered_rand(canvas, min, max);
-            }, 1000);
-            return;
-        }
-
         this.#displayImage(canvas, imageData);
     }
 
-    displayDithered_errDiff(canvas, threshold = 128) {
+    async displayDithered_errDiff(canvas, threshold = 128) {
+        await this.ready;
+
         const imageData = this.#ImageProcessor.getImageData_errDiff(threshold);
-        if (!imageData) {
-            setTimeout(() => {
-                this.displayDithered_errDiff(canvas, threshold);
-            }, 1000);
-            return;
-        }
-
         this.#displayImage(canvas, imageData);
     }
 
-    displayDithered_FloydSteinberg(canvas) {
-        const imageData = this.#ImageProcessor.getImageData_FloydSteinberg();
-        if (!imageData) {
-            setTimeout(() => {
-                this.displayDithered_FloydSteinberg(canvas);
-            }, 1000);
-            return;
-        }
+    async displayDithered_FloydSteinberg(canvas) {
+        await this.ready;
 
+        const imageData = this.#ImageProcessor.getImageData_FloydSteinberg();
         this.#displayImage(canvas, imageData);
     }
 }
-
-/*
-document.querySelector('#apply_filter').addEventListener('click', () => {
-    const img_input = document.querySelector('#img_input');
-    if (img_input.files.length === 0) {
-        alert('Please select an image.');
-        return;
-    }
-
-    const img_url = URL.createObjectURL(img_input.files[0]);
-
-    // Gets first instance of an input with anme "filters" that is checked
-    const filter_radio = document.querySelector('input[name="filters"]:checked');
-    if (filter_radio === null) {
-        alert('Please select a filter');
-        return;
-    }
-
-    const filter = filter_radio.getAttribute('value');
-
-    // Clear canvases
-    const canvases = document.querySelectorAll('canvas');
-    canvases.forEach(canvas => {
-        canvas.remove();
-    });
-
-
-    const filtered_image = new ImageRenderer(img_url);
-    switch (filter) {
-        case 'og':
-            filtered_image.displayOriginal();
-            break;
-        case 'grey':
-            filtered_image.displayGreyscale();
-            break;
-        case 'bw':
-            {
-                let threshold = 128;
-                threshold = Number(prompt('Input threshold: 0 - 255 (inclusive)'));
-                filtered_image.displayBW(threshold);
-            }
-            break;
-        case 'dither_rand':
-            {
-                let lower = 0;
-                let upper = 0;
-                lower = Number(prompt('Input lower bound: 0 - 255 (inclusive)'));
-                upper = Number(prompt('Input upper bound: 0 - 255 (inclusive)'));
-                filtered_image.displayDithered_rand(lower, upper);
-            }
-            break;
-        case 'err_diff':
-            {
-                let threshold = 128;
-                threshold = Number(prompt('Input threshold: 0 - 255 (inclusive)'));
-                filtered_image.displayDithered_errDiff(threshold);
-            }
-            break;
-        case 'floyd_steinberg':
-            {
-                filtered_image.displayDithered_FloydSteinberg();
-            }
-            break;
-
-    }
-});
-*/
 
 export { ImageRenderer };
